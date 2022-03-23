160ff4f75fca11ac2fd99d135a5d4b9cd54a0e77
Yehya Hamzawy <yehya1605639@miuegypt.edu.eg>
2022-03-24 01:00:39 +0200

    used hbs, added simple testing files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,35 @@
 const express = require('express')
+const path = require('path')
+
+console.log(__dirname)
+const publicDir = path.join(__dirname,'../public')
 
 const app = express()
 
+app.set('view engine', 'hbs') // hbs is a handle bar npm package to create a dynamic handle bar. takes advantage of express. express takes it as engine. must write 'view engine'
+app.use(express.static(publicDir))
+
+
+
 app.get('', (req, res) => {
-    res.send('hello express hh')
+    res.render('index', {
+        title: 'No Man\'s Weather',
+        name: 'yaya'
+    }) //renders the handle bar that we set (hbs)
 })
 
 app.get('/help', (req, res) => {
-    res.send('need help? h')
+    res.render('about', {
+     title: 'need help?',
+     name: 'yaya'
+ })
 })
 
 app.get('/about', (req, res) => {
-    res.send('we show weather hh')
+    res.render('about', {
+        title: 'about me',
+        name: 'yaya'
+    })
 })
 
 app.get('/weather', (req, res) => {
